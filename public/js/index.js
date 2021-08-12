7615425247a3fe4ca64fd6518fdb153e446393d4
Unnat <unnat@Unnats-MacBook-Air.local>
2021-08-13 00:43:15 +0530

    custom event emit

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+let socket = io()
+
+socket.on('connect', function () {
+    console.log('connected to server')
+})
+socket.on('disconnect', function () {
+    console.log("disconnected from server")
+})
+
+socket.on('newMessage', function (message) {
+    console.log('newMessage', message)
+})
--- server.js
+++ server.js
@@ -1,11 +1,50 @@
 const express = require("express");
 const app = express()
 const path = require("path")
+const http = require('http')
+const socketIO = require('socket.io');
+
+
 
 const publicPath = path.join(__dirname, './public')
-app.use(express.static(publicPath))
-console.log(publicPath)
+
+let server = http.createServer(app)
+let io = socketIO(server)
+
+
 const port = process.env.PORT || 3030
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("a new user just connected");
+
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: 'Welcome to the chat room!',
+        createdat: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: 'New User joined',
+        createdat: new Date().getTime()
+    })
+
+    socket.on('createMessage', (message) => {
+        console.log(message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdat: new Date().getTime()
+        })
+    })
+    socket.on('disconnect', () => {
+        console.log("user was disconnected")
+    })
+})
+
+
+
+
+
+app.use(express.static(publicPath))
+server.listen(port, () => {
     console.log("your server is up")
 })
