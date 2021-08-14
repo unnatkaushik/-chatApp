6d545ac5a829ebcee8371b40375f991cf9b66e7a
Unnat <unnat@Unnats-MacBook-Air.local>
2021-08-14 13:26:04 +0530

    userlist updated

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,45 @@
 let socket = io()
-
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
 socket.on('connect', function () {
     console.log('connected to server')
+    //var parameterValue = decodeURIComponent(window.location.search.match(/(\?|&)user\=([^&]*)/)[2]);
+    let searchQuery = window.location.search.substring(1)
+    let params = JSON.parse('{"' + decodeURI(searchQuery).replace(/&/g, '","').replace(/\+/g, '').replace(/=/g, '":"') + '"}');
+
+
+    socket.emit('join', params, function (err) {
+
+        if (err) {
+            alert(err),
+                window.location.href = '/'
+        }
+    })
 })
 socket.on('disconnect', function () {
     console.log("disconnected from server")
+})
+socket.on('updateUserList', function (users) {
+    console.log(users)
+    users.forEach(function (user) {
+        $('#chatContactTab').append(`<li class="contacts-item friends active"><a class="contacts-link" href="javascript:;"><div class="contacts-content"><div class="contacts-info"><h6 class="chat-name text-truncate">${user}</h6><div class="chat-time">Just now</div></div></div></a></li>`)
+    })
 })
 
 socket.on('newMessage', function (message) {
     console.log('newMessage', message)
     $("#abcd").append(`<div class="message"><div class="message-wrapper"><div class="message-content"><span>${message.text} </span></div></div></div>`);
 })
-
-
 
 
 socket.on('newLocationMessage', function (message) {
--- server.js
+++ server.js
@@ -4,20 +4,43 @@ const path = require("path")
 const http = require('http')
 const socketIO = require('socket.io');
 const { generateMessage, generateLocationMessage } = require('./utils/message')
-
+const { isRealString } = require('./utils/isRealString')
+const { Users } = require('./utils/users')
 
 const publicPath = path.join(__dirname, './public')
 
 let server = http.createServer(app)
 let io = socketIO(server)
-
+let users = new Users;
 
 const port = process.env.PORT || 3030
 io.on('connection', (socket) => {
     console.log("a new user just connected");
 
-    socket.emit('newMessage', generateMessage("Admin", 'Welcome to the chat room!'));
-    socket.broadcast.emit('newMessage', generateMessage("Admin", 'New User joined'))
+
+    socket.on('join', (params, callback) => {
+        console.log(params)
+        if (!isRealString(params.user) || !isRealString(params.rooms)) {
+            return callback("Please enter a valid details")
+        }
+
+        socket.join(params.rooms);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.user, params.rooms)
+        io.to(params.rooms).emit('updateUserList', users.getUserList(params.rooms));
+
+
+        socket.emit('newMessage', generateMessage("Admin", `Welcome to the ${params.rooms}`));
+        socket.broadcast.to(params.rooms).emit('newMessage', generateMessage("Admin", 'New User joined'))
+
+        callback()
+    })
+
+
+
+
+
+
 
     socket.on('createMessage', (message, callback) => {
         console.log(message);
@@ -29,7 +52,14 @@ io.on('connection', (socket) => {
         io.emit('newLocationMessage', generateLocationMessage("Admin", coords.lat, coords.lng))
     })
     socket.on('disconnect', () => {
-        console.log("user was disconnected")
+        let user = users.removeUser(socket.id);
+
+        console.log("users", user)
+        if (user) {
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage("Admin", `${user.name} has left ${user.room} room`));
+        }
+
     })
 })
 
